cd8321a35799516e6f3c39acf4c84a65bc3fd92b
luiscolon0426 <luiscolon0426@gmail.com>
2021-09-30 18:56:51 +0000

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 
-printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-printf("size of a int: %lu byte(s)\n",  sizeof(ainteger));
-printf("size of a long: %lu byte(s)\n", sizeof(along));
-printf("size of a long long int: %lu byte(s)\n", sizeof(aonglong));
-printf("size of a float: %lu byte(s)\n", sizeof(afloat));
-return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of a int: %lu byte(s)\n",  sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
+
+ return (0);
 }
