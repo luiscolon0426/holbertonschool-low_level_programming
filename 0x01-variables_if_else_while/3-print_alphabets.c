adb09f7a67bde7a379735bfef2f8ab68713035b6
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-01 19:14:15 +0000

    abcABC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int ch, CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}
