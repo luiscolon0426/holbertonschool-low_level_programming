aa5056ab9654827a7083fb138e8dd62e9877f800
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-05 16:44:32 +0000

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alpha in lowercase
+ *
+ * return: 0
+ */
+void print_alphabet(void);
+{
+	(char a);
+	for (char = 'a'; char <= 'z'; char++)
+		_putchar(char)
+		{
+			_putchar('\n')
+		}
+	return (0);
+}
