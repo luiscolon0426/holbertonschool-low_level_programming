88a2e59590844024512d485639b2859c6491a214
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-05 20:10:20 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,25 @@
  * print_sign - Prints th esign of a number
  * @n: The number of wich the sign will be printed.
  *
- * Return: 1 if the number is greater than zero, 0 if the number is zero, -1 if the number is less than zero.
+ * Return: 1 if the number is greater than zero,
+ * 0 if the number is zero,
+ * -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
