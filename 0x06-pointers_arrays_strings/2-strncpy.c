3fa786ba9e29ff64464a3b7cecc57b8adf6895ae
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-14 00:54:03 +0000

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncpy - Copy a string
+ * @dest: destination value.
+ * @src: The source value
+ * @n: The copy limit
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
