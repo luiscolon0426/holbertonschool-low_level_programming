b211316040d95c3bc2fad07526357692965b43bb
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-14 15:33:18 +0000

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,14 +5,14 @@
  * @a: array
  * @n: number of elements of array
  *
- * Return: void
+ *
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 	int temp;
 
-	for (i = 0;i < n--; i++)
+	for (i = 0; i < n--; i++)
 	{
 		temp = a[i];
 		a[i] = a[n];
