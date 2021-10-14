ba371510261ff8160c8027ff157af27e036648ac
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-14 15:47:37 +0000

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *n)
 	int a;
 
 	a = 0;
-	if(n[0] >= 'a' && n[0] <= 'z')
+	if (n[0] >= 'a' && n[0] <= 'z')
 	{
 		n[0] = n[0] - 32;
 	}
@@ -19,24 +19,24 @@ char *cap_string(char *n)
 	{
 		switch (n[a])
 		{
-				case ',':
-				case ';':
-				case '.':
-				case '!':
-				case '?':
-				case '"':
-				case '(':
-				case ')':
-				case '{':
-				case '}':
-				case ' ':
-				case '\n':
-				case '\t':
-				if ( n[a + 1] > 96 && n[a + 1] < 123)
-		{
-				n[a + 1] = n[a + 1] - 32;
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (n[a + 1] > 96 && n[a + 1] < 123)
+				{
+					n[a + 1] = n[a + 1] - 32;
 				}
-				}
-				}
+		}
+	}
 	return (n);
 }
