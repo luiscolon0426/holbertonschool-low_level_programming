d4f868663643460af7ebd6a46538c9e7fa096df6
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-18 20:12:34 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrences of the substriing
+ * @needle: string
+ * @haystack: string
+ *
+ * Return: pointer, or NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return ('\0');
+}
