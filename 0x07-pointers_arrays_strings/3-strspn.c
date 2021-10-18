c8218ab3785a48531aa2b72b9da0420e58a2760b
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-18 17:13:34 +0000

    rename

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of character to match in s
+ * 
+ * Return: the number of bytes in the initial segment
+ * of s wich consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
