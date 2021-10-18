5bd6ae35f0004330c6583f7295840f1c815ccb00
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-18 21:22:32 +0000

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,11 +15,11 @@ void print_diagsums(int *a, int size)
 	int tr = 0;
 	int s = size * size;
 
-	for(i = 0; i < s; i += size + 1)
+	for (i = 0; i < s; i += size + 1)
 	{
 		tl += a[i];
 	}
-	for (i = size - 1; i < s - 1; i += size -1)
+	for (i = size - 1; i < s - 1; i += size - 1)
 	{
 		tr += a[i];
 	}
