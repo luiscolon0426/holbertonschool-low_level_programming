8d3a8a6eaafc904cf024624dd3afd97743c6fbc4
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-22 16:13:34 +0000

    new name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,7 @@
  * main - Prints the program name, followed by anew line.
  * @argc: The number of aguments supplied to the program.
  * @argv: An array of pointers to the argument.
+ *
  * Return: 0
  */
 
@@ -11,7 +12,8 @@ int main(int argc, char *argv[])
 {
 	argc++;
 	argc--;
-	printf("%s\n,", argv[0]);
+
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
