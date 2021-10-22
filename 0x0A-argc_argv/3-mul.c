899ea987bbe8c71407e6b249e9802586a05f41de
luiscolon0426 <luiscolon0426@gmail.com>
2021-10-22 14:49:06 +0000

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -multiply two numbers
+ * @argc: argument count
+ * @argv: array of pointers to argument strings
+ *
+ * Return: result of multplication or 1.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+
+	}
+	/**
+	 * atoi converts a string into an int
+	 */
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
