521c04c2e9611ca0d05009094027bc9a9e9e41f7
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-01 18:49:12 +0000

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,18 +7,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if(d == NULL) /**if statement: if the variable pointer '*d' is equal to 'NULL', excecute line 11*/
+	/*if statement: if the variable pointer 'd' is 'NULL'*/
+	if (d == NULL)
 		return;
-
-	if (d->name == NULL) /**if statement: if 'd->name' is equal to 'NULL', execute line 14*/
-		printf("Name: (nil)\n"); /**print format: "Name:*/
+	/*if statement: if the element 'name' of 'd' is equal to*/
+	 /*NULL*/
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
-	if(d->owner == NULL)
+	/*if statement: if the element 'owner' of 'd' is equal to*/
+	 /*'NULL'*/
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
-
 }
