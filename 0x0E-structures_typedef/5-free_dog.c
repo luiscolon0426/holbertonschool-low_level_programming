188333919c53a9596b71223ba158ccf747d88d2d
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-01 19:24:21 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: struct of dog
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
