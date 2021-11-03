98708a0f6c533194673f6f55146d982d439c89a9
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-03 20:55:57 +0000

    update space

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,11 +8,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t idx;
-	if (*action == NULL)
+
+	if (action == NULL)
 	{
 		return;
 	}
-
 	for (idx = 0; idx < size; idx++)
 		action(array[idx]);
 }
