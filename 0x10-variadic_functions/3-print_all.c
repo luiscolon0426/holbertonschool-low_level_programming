a7508dd23c75c39b2066e5ddd1d20bd5d8c70cd4
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-04 17:52:42 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,5 +14,36 @@ void print_all(const char * const format, ...)
 
 	if (format == NULL)
 	{
-
+		printf("\n");
+		return;
 	}
+	va_start(printList, format);
+	while (format[count2] != '\0')
+	{
+		switch (format[count2])
+		{
+			case 'c':
+				printf("%s%c", comma, va_arg(printList, int));
+				comma = ", ";
+				break;
+			case 'i':
+				printf("%s%d", comma, va_arg(printList, int));
+				comma = ", ";
+				break;
+			case 'f':
+				printf("%s%f", comma, va_arg(printList, double));
+				comma = ", ";
+				break;
+			case 's':
+				string = va_arg(printList, char *);
+				if (string == NULL)
+					string = "(nil)";
+				printf("%s%s", comma, string);
+				comma = ", ";
+				break;
+		}
+		count2++;
+	}
+	printf("\n");
+	va_end(printList);
+}
