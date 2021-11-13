8f77315f9b0d47c209a949a63a0e63e41555ad53
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-13 00:41:26 +0000

    fix hardcode

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - add node list
+ * @head: list
+ * @str: string added to list
+ * Return: pointer
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int idx = 0;
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	if (str == NULL)
+	{
+		tmp->str = NULL;
+		tmp->len = 0;
+	}
+	else
+	{
+		tmp->str = strdup(str);
+		while (str[idx])
+		{
+			idx++;
+		}
+		tmp->len = idx;
+	}
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
