4c2b494bfe949031162c1b6c584bedc52b7df65e
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-16 17:36:10 +0000

    fixed valgrind

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+}
