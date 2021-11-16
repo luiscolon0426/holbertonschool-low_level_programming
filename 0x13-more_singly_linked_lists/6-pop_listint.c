4d3b25c5b1f5eb345f0778d01dec80f71cb5df9e
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-16 18:42:42 +0000

    fixed betty

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 /**
+ * pop_listint - delete a list
+ * @head: head of a list
  *
+ * Return: the head nodes data n
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
 	int n;
 
-	if(head == NULL)
+	if (head == NULL)
 	{
 		return (0);
 	}
 	n = (*head)->n;
 	tmp = (*head)->next;
-	free (*head);
+	free(*head);
 	*head = tmp;
 
 	return (n);
