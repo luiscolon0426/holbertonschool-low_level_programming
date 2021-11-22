529741d108ccc926d6d7f8fc7cf13b2f32a915b1
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-22 19:16:21 +0000

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,12 @@
 #include "main.h"
 /**
- * read_textfile
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: pointer to string
+ * @letters: count letters of string.
+ *
+ * Return: number of letters it could read
+ * and print
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
