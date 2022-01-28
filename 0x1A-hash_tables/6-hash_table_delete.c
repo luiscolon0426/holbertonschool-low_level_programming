b23dc2163b495accce1e0bd1aeb480ca10c2d488
luiscolon0426 <luiscolon0426@gmail.com>
2022-01-28 17:59:37 +0000

    soriano

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,24 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node;
+	unsigned long int i;
+	hash_node_t *node;
 
-    if (ht == NULL || ht->array == NULL || !ht->size)
-        return;
-    for (i = 0; i < ht->size; i++)
-    {
-        while (ht->array[i])
-        {
-            node = ht->array[i]->next;
-            free(ht->array[i]->key);
-            free(ht->array[i]->value);
-            free(ht->array[i]);
-            ht->array[i] = node;
-        }
-    }
-    free(ht->array);
-    ht->array = NULL;
-    ht->size = 0;
-    free(ht);
+	if (ht == NULL || ht->array == NULL || !ht->size)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
