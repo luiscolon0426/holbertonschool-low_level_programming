1ef89d089912c3485f8daec4a270aeb5075552bc
luiscolon0426 <luiscolon0426@gmail.com>
2022-01-28 04:13:39 +0000

    checkers

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,30 +9,30 @@
  **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node = NULL;
-    char *key_cp, *value_cp;
-    unsigned long int idx = 0;
+	hash_node_t *new_node = NULL;
+	char *key_cp, *value_cp;
+	unsigned long int idx = 0;
 
-    if ((strcmp(key, "") == 0 || key == NULL || value == NULL || ht == NULL))
-        return (0);
+	if ((strcmp(key, "") == 0 || key == NULL || value == NULL || ht == NULL))
+		return (0);
 
-    key_cp = strdup(key);
-    value_cp = strdup(value);
+	key_cp = strdup(key);
+	value_cp = strdup(value);
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-    new_node->key = key_cp;
-    new_node->value = value_cp;
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = key_cp;
+	new_node->value = value_cp;
 
-    idx = key_index((const unsigned char *)key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
 
-    if (ht->array[idx] == NULL)
-        new_node->next = NULL;
-    else
-        new_node->next = ht->array[idx];
+	if (ht->array[idx] == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = ht->array[idx];
 
-    ht->array[idx] = new_node;
+	ht->array[idx] = new_node;
 
-    return (1);
+	return (1);
 }
