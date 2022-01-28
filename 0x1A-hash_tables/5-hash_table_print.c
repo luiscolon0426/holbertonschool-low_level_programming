c3f63bbd651713bff2094cc4373f4455e6dcf725
luiscolon0426 <luiscolon0426@gmail.com>
2022-01-28 17:34:00 +0000

    ugh

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ *
+ * @ht: ?
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int first = 1, i = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (!first)
+				printf(", ");
+			else
+				first = 0;
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+		}
+	}
+	printf("}\n");
+}
